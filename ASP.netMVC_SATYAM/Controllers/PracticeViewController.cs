985b98cea59c77b0d133ccdca77b19300d097bdb
satyam-bidve <satyambidve@gmail.com>
2023-08-17 12:03:20 +0530

    View Added

---
+++ ASP.netMVC_SATYAM/Controllers/PracticeViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.netMVC_SATYAM.Controllers
+{
+    public class PracticeViewController : Controller
+    {
+        // GET: PracticeView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
