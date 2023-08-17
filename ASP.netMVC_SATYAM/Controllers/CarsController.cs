a168a00a0b88df1b05adbf155c95e3da5bf5fabd
satyam-bidve <satyambidve@gmail.com>
2023-08-17 12:29:30 +0530

    Model Addded

---
+++ ASP.netMVC_SATYAM/Controllers/CarsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.netMVC_SATYAM.Models; // need this import 
+
+namespace ASP.netMVC_SATYAM.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Index()
+        {
+            var data = GetCars();
+
+            return View(data);
+        }
+
+        // in this method commonly we will get data from database store it in any collection class and then pass to the ActionMethod then to 
+        // view as responce 
+        private Cars GetCars()
+        {
+            return new Cars()
+            {
+                carID = 101,
+                carName = "Supra",
+                brand = "toyota"
+            };
+        }
+    }
+}
---
+++ ASP.netMVC_SATYAM/Models/Cars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.netMVC_SATYAM.Models
+{
+    // This is the model class that represent the data 
+    public class Cars
+    {
+        public int carID { get; set; }
+
+        public string carName { get; set; }
+
+        public string brand { get; set; }
+    }
+}
